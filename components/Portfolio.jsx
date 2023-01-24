37734d03e6217b7401c45a689ea8d66309727ad5
Enzo Holgado <enzoholgadodev@gmail.com>
2023-01-24 10:42:40 -0500

    actualizando los proyectos

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -1,15 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
-import firstProject from "../public/firstProject.gif";
-import animercce from "../public/animercce.gif";
+import React from "react";
 import project1 from "../public/project1.png";
 import project2 from "../public/project2.png";
+import { BiLink } from "react-icons/bi";
 
 const Portfolio = () => {
-  const [projectOne, setProjectOne] = useState(project1);
-  const [projectTwo, setProjectTwo] = useState(project2);
-
   return (
     <section
       className="flex flex-col justify-evenly items-center w-full min-h-screen px-5 gap-20 "
@@ -21,40 +17,54 @@ const Portfolio = () => {
       >
         PORTAFOLIO
       </h1>
-      <div  className=" flex flex-wrap gap-10 justify-center">
-        <Link
-          href="https://first-portfolio-template.vercel.app/"
-          target="_blank"
-          className=" flex flex-col items-center lg:w-2/6 transition-all gap-5 "
-          onMouseOver={() => setProjectOne(firstProject)}
-          onMouseLeave={() => setProjectOne(project1)}
-        >
+      <article className=" flex flex-wrap gap-10 justify-center">
+        <div className=" flex  rounded-md justify-between w-5/6">
           <Image
-            src={projectOne}
-            className="w-full h-full shadow-md rounded-md hover:border-b-4 hover:border-teal-500 transition-all"
+            src={project1}
+            className="w-2/6 h-full shadow-md rounded-md transition-all"
             alt="project one"
           />
-          <p className=" text-gray-200 ">
-            Haz click en la imagen para ver el proyecto
-          </p>
-        </Link>
-        <Link
-          href="https://animercce.vercel.app/"
-          target="_blank"
-          className=" flex flex-col items-center lg:w-2/6 transition-all gap-5 "
-          onMouseOver={() => setProjectTwo(animercce)}
-          onMouseLeave={() => setProjectTwo(project2)}
-        >
-          <Image
-            src={projectTwo}
-            className="w-full h-full shadow-md rounded-md text-teal-50 tracking-wider hover:border-b-4 hover:border-teal-500 transition-all"
-            alt="project two"
-          />
-          <p className=" text-gray-200 ">
-            Haz click en la imagen para ver el proyecto
-          </p>
-        </Link>
-      </div>
+          <div className=" w-5/6 flex flex-col justify-between items-center px-4 ">
+            <h3 className=" text-gray-900 drop-shadow-md font-bold text-xl">Portfolio template</h3>
+            <p className=" w-full text-gray-900 drop-shadow-md ">
+              Tengo conocimientos en lenguajes como:{" "}
+              <span className=" font-semibold text-teal-500 ">
+                JavaScript, Python, TypeScript, HTML, CSS, SQL y noSQL.
+              </span>{" "}
+              con el uso de frameworks y librerias como{" "}
+              <span className=" font-semibold text-teal-500 ">
+                React.js, Next.js, Redux, Tailwind css
+              </span>{" "}
+              para el lado del frontend. Y{" "}
+              <span className=" font-semibold text-teal-500 ">
+                Node.js, Express.js, Django, Sequelize, PostgreSQL, MySQL,
+                MongoDB, Mongoose
+              </span>{" "}
+              para backend y base de datos. ¡Entre otras!
+            </p>
+            <Link
+              href="https://first-portfolio-template.vercel.app/"
+              target="_blank"
+              className=" flex items-center gap-2 self-end bg-gradient-to-r transition-all from-fuchsia-500 to-orange-600 px-1 shadow-md rounded-sm py-px text-teal-50 tracking-wide hover:scale-105 "
+            >
+              Ver proyecto <BiLink />
+            </Link>
+          </div>
+        </div>
+        {/* <div>
+          <Link
+            href="https://animercce.vercel.app/"
+            target="_blank"
+            className=" flex flex-col items-center lg:w-2/6 transition-all gap-5 "
+          >
+            <Image
+              src={project2}
+              className="w-full h-full shadow-md rounded-md text-teal-50 tracking-wider hover:border-b-4 hover:border-teal-500 transition-all"
+              alt="project two"
+            />
+          </Link>
+        </div> */}
+      </article>
     </section>
   );
 };
