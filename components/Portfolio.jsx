e7dcf60768ef56815a8eb7f932e90126a5aadff0
Enzo Holgado <enzoholgadodev@gmail.com>
2023-01-24 11:01:39 -0500

    corrigiendo posicionamientos y mas cosas de los proyectos

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -8,7 +8,7 @@ import { BiLink } from "react-icons/bi";
 const Portfolio = () => {
   return (
     <section
-      className="flex flex-col justify-evenly items-center w-full min-h-screen px-5 gap-20 "
+      className="flex flex-col justify-evenly items-center w-full min-h-screen px-5 py-10 gap-20 "
       id="portfolio"
     >
       <h1
@@ -17,14 +17,90 @@ const Portfolio = () => {
       >
         PORTAFOLIO
       </h1>
-      <article className=" flex flex-wrap gap-10 justify-center">
-        <div className=" flex  rounded-md justify-between w-5/6">
+      <article className=" flex flex-wrap gap-20 justify-center">
+        <div className=" flex gap-5 rounded-md justify-between w-5/6">
           <Image
             src={project1}
+            data-aos="fade-right"
             className="w-2/6 h-full shadow-md rounded-md transition-all"
             alt="project one"
           />
-          <div className=" w-5/6 flex flex-col justify-between items-center px-4 ">
+          <div
+            data-aos="fade-in"
+            className=" w-5/6 flex flex-col justify-between items-center px-4 "
+          >
+            <h3 className=" text-gray-900 drop-shadow-md font-bold text-2xl">
+              Portfolio template
+            </h3>
+            <p className=" text-xl w-full text-gray-900 drop-shadow-md ">
+              Este es un proyecto individual donde realicé un template de un
+              portafolio con tecnologías como:{" "}
+              <span className=" font-semibold text-teal-500 ">
+                React, Next.js, Tailwind css y React-icons.
+              </span>{" "}
+              El enfoque de este proyecto es que sea intuitivo y fácil de leer,
+              además de haber implementado una funcionalidad de{" "}
+              <span className=" font-semibold text-teal-500 ">
+                Dark and Light themes
+              </span>{" "}
+              para la comodidad del usuario.
+            </p>
+            <Link
+              href="https://first-portfolio-template.vercel.app/"
+              target="_blank"
+              className=" flex items-center gap-2 self-end bg-gradient-to-r transition-all from-fuchsia-500 to-orange-600 px-2 shadow-md rounded-sm py-1 text-teal-50 tracking-wider hover:scale-110"
+            >
+              Ver proyecto <BiLink />
+            </Link>
+          </div>
+        </div>
+        <div className=" flex  rounded-md justify-between w-5/6">
+          <div
+            data-aos="fade-in"
+            className=" w-5/6 flex flex-col justify-between items-center px-4 "
+          >
+            <h3 className=" text-gray-900 drop-shadow-md font-bold text-2xl">
+              Portfolio template
+            </h3>
+            <p className=" text-xl w-full text-gray-900 drop-shadow-md ">
+              Este es un proyecto individual donde realicé un template de un
+              portafolio con tecnologías como:{" "}
+              <span className=" font-semibold text-teal-500 ">
+                React, Next.js, Tailwind css y React-icons.
+              </span>{" "}
+              El enfoque de este proyecto es que sea intuitivo y fácil de leer,
+              además de haber implementado una funcionalidad de{" "}
+              <span className=" font-semibold text-teal-500 ">
+                Dark and Light themes
+              </span>{" "}
+              para la comodidad del usuario.
+            </p>
+            <Link
+              href="https://first-portfolio-template.vercel.app/"
+              target="_blank"
+              className=" flex items-center gap-2 self-end bg-gradient-to-r transition-all from-fuchsia-500 to-orange-600 px-2 shadow-md rounded-sm py-1 text-teal-50 tracking-wider hover:scale-110"
+            >
+              Ver proyecto <BiLink />
+            </Link>
+          </div>
+          <Image
+            src={project1}
+            data-aos="fade-left"
+            className="w-2/6 h-full shadow-md rounded-md transition-all"
+            alt="project one"
+          />
+        </div>
+        <div className=" flex  rounded-md justify-between w-5/6">
+          <Image
+            src={project1}
+            data-aos="fade-right"
+            className="w-2/6 h-full shadow-md rounded-md transition-all"
+            alt="project one"
+          />
+          <div
+            data-aos="fade-in"
+            className=" w-5/6 flex flex-col justify-between items-center px-4 "
+          >
             <h3 className=" text-gray-900 drop-shadow-md font-bold text-2xl">
               Portfolio template
             </h3>
